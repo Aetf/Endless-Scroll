537390017d857fd450223a8df6da84f34cf8772c
Aetf <7437103@gmail.com>
2015-08-10 21:27:18 +0800

    working bounded camera, scrollable background. Player can jump now

---
+++ Assets/Core/Scripts/EndlessGroundGenerator.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class EndlessGroundGenerator : MonoBehaviour {
+
+    public GameObject groundTile;
+    public float threshould = 1f;
+    public int tilesPerGen = 3;
+    public float tileWidth = 8.0f;
+    public float tileHeight = 4.0f;
+
+    private const float groundAltitude = 0f;
+
+    private Transform playerTrans;
+
+    private EdgeCollider2D edge;
+
+    private List<GameObject> tiles = new List<GameObject>();
+    private float leftEdge = 0f;
+    private float rightEdge = 0f;
+
+	// Use this for initialization
+	void Start () {
+        edge = GetComponent<EdgeCollider2D>();
+        playerTrans = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GenerateGroundAround(playerTrans.position.x);
+	}
+
+    void GenerateGroundAround(float x) {
+        if (!InTilesRange(x)) {
+            Debug.Log(string.Format("Warning: generating ground around {0} which is outside current ground, this is not supported now.", x));
+        }
+
+        if (NearLeftEdge(x)) {
+            CreateTilesWithRightEdgeAt(new Vector2(leftEdge, groundAltitude), tilesPerGen);
+        }
+        if (NearRightEdge(x)) {
+            CreateTilesWithLeftEdgeAt(new Vector2(rightEdge, groundAltitude), tilesPerGen);
+        }
+    }
+
+    void CreateTilesWithLeftEdgeAt(Vector2 pos, int tilesCount) {
+        Vector2 center = new Vector2(pos.x + tileWidth / 2, pos.y);
+        while (tilesCount > 0) {
+            CreateTileAt(center);
+            center.x += tileWidth;
+            --tilesCount;
+        }
+    }
+
+    void CreateTilesWithRightEdgeAt(Vector2 pos, int tilesCount) {
+        Vector2 center = new Vector2(pos.x - tileWidth / 2, pos.y);
+        while (tilesCount > 0) {
+            CreateTileAt(center);
+            center.x -= tileWidth;
+            --tilesCount;
+        }
+    }
+
+    void CreateTilesCentering(Vector2 pos) {
+        if (tilesPerGen % 2 == 0) {
+            int tilesOnLeft = tilesPerGen / 2;
+            int tilesOnRight = tilesOnLeft;
+
+            CreateTilesWithLeftEdgeAt(pos, tilesOnRight);
+            CreateTilesWithRightEdgeAt(pos, tilesOnLeft);
+        } else {
+            int tilesOnLeft = tilesPerGen / 2;
+            int tilesOnRight = tilesOnLeft;
+
+            CreateTileAt(pos);
+            CreateTilesWithLeftEdgeAt(new Vector2(pos.x + tileWidth / 2, pos.y), tilesOnRight);
+            CreateTilesWithRightEdgeAt(new Vector2(pos.x - tileWidth / 2, pos.y), tilesOnLeft);
+        }
+    }
+
+    GameObject CreateTileAt(Vector2 position) {
+        Debug.Log("Creating tile at " + position);
+
+        GameObject tile = Instantiate(groundTile,
+            new Vector3(position.x, position.y, 0f),
+            Quaternion.identity) as GameObject;
+        tile.transform.parent = transform;
+
+        float right = position.x + tileWidth / 2;
+        float left = position.x - tileWidth / 2;
+        if (right > rightEdge)
+            rightEdge = right;
+        if (left < leftEdge)
+            leftEdge = left;
+        UpdateEdgeCollider();
+
+        tiles.Add(tile);
+        return tile;
+    }
+
+    void UpdateEdgeCollider() {
+        Vector2[] points = edge.points;
+        points[0].x = leftEdge;
+        points[1].x = rightEdge;
+        edge.points = points;
+    }
+
+    bool InTilesRange(float x) {
+        return x >= leftEdge && x <= rightEdge;
+    }
+
+    bool NearLeftEdge(float x) {
+        return x >= leftEdge && x <= leftEdge + threshould;
+    }
+
+    bool NearRightEdge(float x) {
+        return x <= rightEdge && x >= rightEdge - threshould;
+    }
+}
---
+++ Assets/Core/Scripts/OffsetMaterial.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnlimitedCodeWorks;
+using UnlimitedCodeWorks.Events.Character;
+
+public class OffsetMaterial : MonoBehaviour {
+
+    public float scrollSpeed = 0.3f;
+
+    private float camHorzExtent;
+    private float camVertExtent;
+
+    private Renderer groundRenderer;
+    private Transform playerTrans;
+
+    private float scrollPosition;
+    private float lastX;
+
+    // Use this for initialization
+    void Start () {
+        // Setup references
+        groundRenderer = transform.GetChild(0).gameObject.GetComponent<Renderer>();
+        playerTrans = GameObject.FindGameObjectWithTag("Player").transform;
+
+        // Set main camera bounds
+        Camera.main.GetComponent<CameraFollow>().SetMovingBounds(groundRenderer.bounds);
+
+        // Set and apply initial values
+        scrollPosition = 0f;
+        lastX = playerTrans.position.x;
+        Scroll(0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        TrackPlayer();
+	}
+
+    void TrackPlayer() {
+        float delta = playerTrans.position.x - lastX;
+        lastX = playerTrans.position.x;
+        Scroll(delta);
+    }
+
+    void Scroll(float delta) {
+        scrollPosition += delta * scrollSpeed;
+
+        float x = Mathf.Repeat(scrollPosition, 1);
+        Vector2 offset = new Vector2(x, 0);
+        groundRenderer.sharedMaterial.mainTextureOffset = offset;
+    }
+}
