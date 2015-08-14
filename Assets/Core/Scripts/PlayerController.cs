b9f76bfb49fa56d03f1d8b0a5d0c26c9640869cf
Aetf <7437103@gmail.com>
2015-08-15 00:48:35 +0800

    More advanced player controller

--- Assets/Core/Scripts/PlayerController.cs
+++ Assets/Core/Scripts/PlayerController.cs
@@ -1,117 +1,31 @@
-﻿using UnityEngine;
-using System.Collections;
-using UnlimitedCodeWorks;
-using UnlimitedCodeWorks.Events.Character;
+﻿using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
+[RequireComponent(typeof(CharacterCore))]
 public class PlayerController : MonoBehaviour {
+    private CharacterCore m_Character;
+    private bool m_Jump;
 
-	public float moveForce = 365f;			// Amount of force added to move the player left and right.
-	public float maxSpeed = 5f;				// The fastest the player can travel in the x axis.
-	public float jumpForce = 1000f;			// Amount of force added when the player jumps.
-
-
-    private Rigidbody2D rb2d;               // Cache frequently used component reference.
-	private Transform groundCheck;			// A position marking where to check if the player is grounded.
-	private bool grounded = false;			// Whether or not the player is grounded.
-	private Animator anim;					// Reference to the player's animator component.
-
-	[HideInInspector]
-	public bool facingRight = true;			// For determining which way the player is currently facing.
-	[HideInInspector]
-	public bool jump = false;				// Condition for whether the player should jump.
-
-	// Use this for initialization
-	void Start () {
-        rb2d = GetComponent<Rigidbody2D>();
-		groundCheck = transform.Find("groundCheck");
-        // force a change event to get everything in sync
-        EventHub.RaiseEvent(new PlayerFacingChanged(gameObject, facingRight));
+    private void Awake() {
+        m_Character = GetComponent<CharacterCore>();
     }
 
-    // 0..n times per frame before physical simulation
-    void FixedUpdate() {
-		// Cache the horizontal input.
-		float h = Input.GetAxis("Horizontal");
 
-		// The Speed animator parameter is set to the absolute value of the horizontal input.
-		//anim.SetFloat("Speed", Mathf.Abs(h));
-
-		// If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
-		if(h * rb2d.velocity.x < maxSpeed)
-			// ... add a force to the player.
-			rb2d.AddForce(Vector2.right * h * moveForce);
-
-		// If the player's horizontal velocity is greater than the maxSpeed...
-		if(Mathf.Abs(rb2d.velocity.x) > maxSpeed)
-			// ... set the player's velocity to the maxSpeed in the x axis.
-			rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
-
-		// If the input is moving the player right and the player is facing left...
-		if(h > 0 && !facingRight)
-			// ... flip the player.
-			Flip();
-		// Otherwise if the input is moving the player left and the player is facing right...
-		else if(h < 0 && facingRight)
-			// ... flip the player.
-			Flip();
-
-		// If the player should jump...
-		if(jump)
-		{
-			// Set the Jump animator trigger parameter.
-			//anim.SetTrigger("Jump");
-
-			// Play a random jump audio clip.
-			//int i = Random.Range(0, jumpClips.Length);
-			//AudioSource.PlayClipAtPoint(jumpClips[i], transform.position);
-
-			// Add a vertical force to the player.
-			rb2d.AddForce(new Vector2(0f, jumpForce));
-
-			// Make sure the player can't jump again until the jump conditions from Update are satisfied.
-			jump = false;
-		}
-    }
-
-	// Update is called once per frame, after rendering
-	void Update () {
-		// The player is grounded if a linecast to the groundcheck position hits anything on the ground layer.
-		grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
-
-		// If the jump button is pressed and the player is grounded then the player should jump.
-		if(Input.GetButtonDown("Jump") && grounded)
-			jump = true;
-
-        // Raise moved event for anyone who needs tracking the player.
-        EventHub.RaiseEvent(new PlayerMoved(gameObject));
-	}
-
-    bool ShouldMove(Vector3 target) {
-        PlayerMoving evt = new PlayerMoving(gameObject, target);
-        EventHub.RaiseEvent(evt);
-        return !evt.Cancel;
-    }
-
-	void Flip ()
-	{
-		// Switch the way the player is labeled as facing.
-		facingRight = !facingRight;
-
-		// Multiply the player's x local scale by -1.
-		Vector3 theScale = transform.localScale;
-		theScale.x *= -1;
-		transform.localScale = theScale;
-
-
-        // Raise facing changed event
-        EventHub.RaiseEvent(new PlayerFacingChanged(gameObject, facingRight));
-	}
-
-    void Move(float delta) {
-        Vector3 target = transform.position + new Vector3(delta, 0, 0);
-        if (ShouldMove(target)) {
-            transform.Translate(delta, 0, 0);
-            EventHub.RaiseEvent(new PlayerMoved(gameObject));
+    private void Update() {
+        if (!m_Jump) {
+            // Read the jump input in Update so button presses aren't missed.
+            m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
         }
+    }
+
+
+    private void FixedUpdate() {
+        // Read the inputs.
+        bool crouch = Input.GetKey(KeyCode.LeftControl);
+        float h = CrossPlatformInputManager.GetAxis("Horizontal");
+        // Pass all parameters to the character core script.
+        m_Character.Move(h, crouch, m_Jump);
+        m_Jump = false;
     }
 }
